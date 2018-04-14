00e4a43d0cde0fa6b27abafce9fddc486e5264c7
Jens <jenszza@gmail.com>
2018-04-14 14:17:55 +0200

    Added files

---
+++ code/matlab/MalflietTjon.m
@@ -0,0 +1,19 @@
+function V = MalflietTjon(r)
+% Malfliet-Tjon-potential
+% r: fm
+% V: MeV
+l1 = -586.04;
+l2 = 1458.19;
+l3 = -872.15;
+
+mu1 = 1.55;
+mu2 = 3.11;
+mu3 = 6.00;
+
+V1 = l1*exp(-mu1.*r);
+V2 = l2*exp(-mu2.*r);
+V3 = l3*exp(-mu3.*r);
+
+V = (V1 + V2 + V3)./r;
+end
+
